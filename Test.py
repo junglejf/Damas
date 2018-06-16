f215c024287a36ba5722c93f38c0a85639945689
jorgefcc <jfcchagas@id.uff.br>
2018-06-16 10:33:31 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Test.py
 import unittest
 from Damas.Damas import *
 
@@ -62,58 +61,5 @@ class testJogar(unittest.TestCase):
     def Inicializa(self):
         self.jogo.assertEqual(self.jogo.tabuleiro,tabuleiro, msg = 'inicializou corretamente')
 
-
-=======
-from unittest import TestCase
-from Damas import *#_testes import *
-
-class TestJogo(TestCase):
-    def test1_verifica_vencedor(self):
-        sair = False
-        jogo = Jogo()
-        #####################GARANTINDO QUE OCORRA A CONDIÇÃO 1 DO TESTE
-        # fazendo com que resp1<-randomIA<-none
-        assert (jogo.tabuleiro==[
-                          ['-', '-', '-', '-', '-', '-', '-', '-'],
-                          ['-', '-', '-', '-', '-', '-', '-', '-'],
-                          ['-', '-', '-', '-', '-', '-', '-', '-'],
-                          ['-', '-', '-', '-', '-', '-', '-', 'x'],
-                          ['-', '-', '-', '-', '-', '-', 'o', '-'],
-                          ['-', '-', '-', '-', '-', 'o', '-', '-'],
-                          ['-', '-', '-', '-', '-', '-', '-', '-'],
-                          ['-', '-', '-', '-', '-', '-', '-', '-']
-        ])
-
-        while not sair:
-            if jogo.jogadores[jogo.turno % 2] == 'o':
-                for evento in pygame.event.get():
-                    if evento.type == pygame.QUIT:
-                        sair = True
-                        pygame.quit()
-                        quit()
-                    if evento.type == pygame.MOUSEBUTTONDOWN:
-                        jogo.jogadas(pygame.mouse.get_pos())
-            else:
-                # jogo.cedula_selecionada = None
-                time.sleep(0.5)
-                jogo = IAsimples(jogo, 1)
-                jogo.proximo_turno()
-
-            tela.fill(PRETO)
-            jogo.desenha()
-
-            vencedor = jogo.verifica_vencedor()
-
-            if vencedor is not None:
-                fim_de_jogo(vencedor)
-                sair = True
-
-            pygame.display.update()
-            clock.tick(60)
-
-        ### como garantir que a função verifica_vencedor aqui retorne o que se quer ?
-        ###
-        #self.fail()
-        return True
 
 
